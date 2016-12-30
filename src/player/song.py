90f5381b8d5d631c986cf684749b681fce9acdfd
Florian Fischer <florian.fl.fischer@fau.de>
2016-12-30 20:42:03 +0100

    remove logging and error handling

--- src/player/song.py
+++ src/player/song.py
@@ -1,38 +1,26 @@
-import os, sys
-import logging
-
-sys.path.insert(0,"..")
-import WRMSconf
 from comparableMixin import ComparableMixin
 
-music_dir = WRMSconf.music_dir
 
 class Song(ComparableMixin):
     path = ""
     weight, upvotes, downvotes = (0,0,0)
 
     def __init__(self, path):
-        if not os.path.isfile(music_dir + '/' + path):
-            raise IOError("no file associated")
         self.path = path
-        logging.debug(str(self) + "created")
 
     def _cmpkey(self):
         return self.weight, self.path
 
     def __repr__(self):
         return "(Song: {0}; Weight: {1})".format(self.path, self.weight)
-	# Printing the rank (not the weight) should now be done by the playlist
 
     def upvote(self):
         self.weight += 1
         self.upvotes += 1
-        logging.debug(str(self) + " upvoted")
 
     def downvote(self):
         self.weight -= 1
         self.downvotes += 1
-        logging.debug(str(self) + " downvoted")
 
     def get_weight(self):
         return self.weight
