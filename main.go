fischerling/WRMS

commit d8bdce51c336f3693121202402ee38ff3f12c0e2

send bad request status if the websocket connection has a bad UUID cookie

main.go
@@ -208,11 +208,15 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	uuidCookie, err := r.Cookie("UUID")
 	if err != nil {
 		llog.Error("websocket connection has no set UUID cookie")
+		http.Error(w, "websocket connection has no set UUID cookie", http.StatusBadRequest)
+		return
 	}
 
 	id, err := uuid.Parse(uuidCookie.Value)
 	if err != nil {
 		llog.Error("Invalid UUID set in cookie")
+		http.Error(w, "Invalid UUID set in cookie", http.StatusBadRequest)
+		return
 	}
 
 	defer func() {

commit 0d0b416146dd146a5dcfbf61f09f8136aa5afdc2

create websocket context after successfully identifying the connection
Fixes: d8bdce51c336f3693121202402ee38ff3f12c0e2

main.go
@@ -201,10 +201,6 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ctx, cancel := context.WithCancel(r.Context())
-
-	ctx = c.CloseRead(ctx)
-
 	uuidCookie, err := r.Cookie("UUID")
 	if err != nil {
 		llog.Error("websocket connection has no set UUID cookie")
@@ -218,6 +214,9 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid UUID set in cookie", http.StatusBadRequest)
 		return
 	}
+
+	ctx, cancel := context.WithCancel(r.Context())
+	ctx = c.CloseRead(ctx)
 
 	defer func() {
 		llog.Info("cancel context of connection %v", id)