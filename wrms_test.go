fischerling/WRMS

commit 26f0033ebb5f8e7c204a042709e3dfa885eac868

fix applyTimeBonus by always using Song pointers
Storing song objects in the Songs slice and using pointers to the
underlying array slots breaks if the underlying array must be grown.
When the array is grown the objects are copied to the new array and
the pointers used in queue point to the old objects.
This causes that Playlist.applyTimeBonus may not correctly apply
to the most recent list of songs.

This is fixed by always using Song pointers.

playlist.go
@@ -78,7 +78,8 @@ func (pl *Playlist) OrderedList() []*Song {
 func (pl *Playlist) applyTimeBonus(timeBonus float64) {
 	for _, s := range *pl {
 		s.Weight += timeBonus
+		llog.DDebug("%p %v", s, s.Weight)
 	}
 	heap.Init(pl)
-	llog.DDebug("applying time bonus to the playlist")
+	llog.DDebug("applying time bonus to the playlist %v", *pl)
 }

wrms_test.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"io"
-	"muhq.space/go/wrms/llog"
 	"testing"
 )
 
 type mockPlayer struct{}
 
-func (p *mockPlayer) Play(*Song)                                         {}
-func (p *mockPlayer) PlayUri(string)                                     {}
-func (p *mockPlayer) PlayData(io.Reader)                                 {}
-func (p *mockPlayer) Search(pattern map[string]string) (res chan []Song) { return }
-func (p *mockPlayer) Playing() bool                                      { return false }
-func (p *mockPlayer) Pause()                                             {}
-func (p *mockPlayer) Continue()                                          {}
-func (p *mockPlayer) Stop()                                              {}
+func (p *mockPlayer) Play(*Song)                                          {}
+func (p *mockPlayer) PlayUri(string)                                      {}
+func (p *mockPlayer) PlayData(io.Reader)                                  {}
+func (p *mockPlayer) Search(pattern map[string]string) (res chan []*Song) { return }
+func (p *mockPlayer) Playing() bool                                       { return false }
+func (p *mockPlayer) Pause()                                              {}
+func (p *mockPlayer) Continue()                                           {}
+func (p *mockPlayer) Stop()                                               {}
 
 var alice, _ = uuid.NewRandom()
 
@@ -33,7 +33,7 @@ func TestWrmsAdd(t *testing.T) {
 
 func TestWrmsAdd3Next3(t *testing.T) {
 	wrms := Wrms{Player: &mockPlayer{}}
-	songs := []Song{
+	songs := []*Song{
 		NewDummySong("song1", "snfmt"),
 		NewDummySong("song2", "snfmt"),
 		NewDummySong("song3", "snfmt"),
@@ -94,7 +94,6 @@ func TestWrmsUpUpDownNextNext(t *testing.T) {
 	wrms.AddSong(s2)
 	wrms.AddSong(s3)
 
-	llog.Info("UpUpDown test")
 	wrms.AdjustSongWeight(alice, s1.Uri, "up")
 	wrms.AdjustSongWeight(alice, s2.Uri, "down")
 	wrms.AdjustSongWeight(alice, s3.Uri, "up")
@@ -114,7 +113,6 @@ func TestWrmsDoubleAdd(t *testing.T) {
 	wrms := Wrms{Player: &mockPlayer{}}
 	s1 := NewDummySong("song1", "snfmt")
 
-	llog.Info("Double Add test")
 	wrms.AddSong(s1)
 	wrms.AdjustSongWeight(alice, s1.Uri, "up")
 	wrms.Next()
@@ -138,6 +136,26 @@ func TestFlipVote(t *testing.T) {
 	wrms.AdjustSongWeight(alice, s.Uri, "down")
 	if wrms.Songs[0].Weight != -1 {
 		t.Log("Weight is not -1")
+		t.Fail()
+	}
+}
+
+func TestTimeBonus(t *testing.T) {
+	wrms := Wrms{Player: &mockPlayer{}}
+	wrms.Config.timeBonus = 0.1
+	songs := []*Song{
+		NewDummySong("song1", "snfmt"),
+		NewDummySong("song2", "snfmt"),
+		NewDummySong("song3", "snfmt"),
+	}
+
+	for _, s := range songs {
+		wrms.AddSong(s)
+	}
+
+	exp := 0.1 * float64(len(songs)-1)
+	if wrms.Songs[0].Weight != exp {
+		t.Log(fmt.Sprintf("TimeBonus not correctly applied song %p %v expected: %v", &wrms.Songs[0], wrms.Songs[0], exp))
 		t.Fail()
 	}
 }

backend.go
@@ -7,7 +7,7 @@ import (
 
 type Backend interface {
 	Play(song *Song, player Player)
-	Search(map[string]string) []Song
+	Search(map[string]string) []*Song
 	OnSongFinished(song *Song)
 }
 
@@ -15,12 +15,12 @@ type DummyBackend struct{}
 
 func (dummy *DummyBackend) Play(song *Song, player Player) {}
 func (dummy *DummyBackend) OnSongFinished(song *Song)      {}
-func (dummy *DummyBackend) Search(map[string]string) []Song {
+func (dummy *DummyBackend) Search(map[string]string) []*Song {
 	s := NewDummySong("Dummy Mc Crashtest", "foo")
-	return []Song{s}
+	return []*Song{s}
 }
 
-func NewDummySong(title, artist string) Song {
+func NewDummySong(title, artist string) *Song {
 	h := sha1.New()
 	h.Write([]byte(title))
 	h.Write([]byte(artist))

localBackend.go
@@ -114,7 +114,7 @@ func (b *LocalBackend) findSongs() {
 		s := NewSong(m.Title(), m.Artist(), "local", p)
 		s.Album = m.Album()
 		s.Year = m.Year()
-		songs = append(songs, &s)
+		songs = append(songs, s)
 		return nil
 	})
 
@@ -129,7 +129,7 @@ func (b *LocalBackend) Play(song *Song, player Player) {
 	player.PlayUri("file://" + song.Uri)
 }
 
-func (b *LocalBackend) Search(patterns map[string]string) (results []Song) {
+func (b *LocalBackend) Search(patterns map[string]string) (results []*Song) {
 	pattern := strings.ToLower(patterns["pattern"])
 
 	query := fmt.Sprintf("SELECT * FROM songs WHERE Title LIKE '%%%s%%' OR Artist LIKE '%%%s%%'",

player.go
@@ -14,7 +14,7 @@ import (
 type Player interface {
 	Play(*Song)
 	Playing() bool
-	Search(map[string]string) chan []Song
+	Search(map[string]string) chan []*Song
 	PlayUri(string)
 	PlayData(io.Reader)
 	Pause()
@@ -232,11 +232,11 @@ func (player *MpvPlayer) _stop() {
 	player.mpv.Store(nil)
 }
 
-func (player *MpvPlayer) Search(pattern map[string]string) chan []Song {
+func (player *MpvPlayer) Search(pattern map[string]string) chan []*Song {
 	var wg sync.WaitGroup
 	wg.Add(len(player.Backends))
 
-	ch := make(chan []Song)
+	ch := make(chan []*Song)
 
 	for _, backend := range player.Backends {
 		backend := backend

playlist_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestPlAdd(t *testing.T) {
 	var pl Playlist
 	s := NewDummySong("Foo", "Bar")
-	pl.Add(&s)
+	pl.Add(s)
 	if len(pl) != 1 {
 		t.Log("len should be 1")
 		t.Fail()
@@ -15,7 +15,7 @@ func TestPlAdd(t *testing.T) {
 func TestPlAddPop(t *testing.T) {
 	var pl Playlist
 	s := NewDummySong("Foo", "Bar")
-	pl.Add(&s)
+	pl.Add(s)
 	if len(pl) != 1 {
 		t.Log("len should be 1")
 		t.Fail()
@@ -27,7 +27,7 @@ func TestPlAddPop(t *testing.T) {
 		t.Fail()
 	}
 
-	if ps != &s {
+	if ps != s {
 		t.Log("the popped ptr should match the one added")
 		t.Fail()
 	}
@@ -37,8 +37,8 @@ func TestPlAddPopTwice(t *testing.T) {
 	var pl Playlist
 	s1 := NewDummySong("Foo", "Bar")
 	s2 := NewDummySong("Nasen", "Baer")
-	pl.Add(&s1)
-	pl.Add(&s2)
+	pl.Add(s1)
+	pl.Add(s2)
 	if len(pl) != 2 {
 		t.Log("len should be 2")
 		t.Fail()
@@ -51,12 +51,12 @@ func TestPlAddPopTwice(t *testing.T) {
 		t.Fail()
 	}
 
-	if ps1 != &s1 {
+	if ps1 != s1 {
 		t.Log("the popped ptr should match the one added")
 		t.Fail()
 	}
 
-	if ps2 != &s2 {
+	if ps2 != s2 {
 		t.Log("the popped ptr should match the one added")
 		t.Fail()
 	}
@@ -66,15 +66,15 @@ func TestPlAdd2AdjustPop(t *testing.T) {
 	var pl Playlist
 	s1 := NewDummySong("Foo", "Bar")
 	s2 := NewDummySong("Nasen", "Baer")
-	pl.Add(&s1)
-	pl.Add(&s2)
+	pl.Add(s1)
+	pl.Add(s2)
 	if len(pl) != 2 {
 		t.Log("len should be 2")
 		t.Fail()
 	}
 
 	s2.Weight += 1
-	pl.Adjust(&s2)
+	pl.Adjust(s2)
 
 	ps := pl.PopSong()
 	if len(pl) != 1 {
@@ -82,7 +82,7 @@ func TestPlAdd2AdjustPop(t *testing.T) {
 		t.Fail()
 	}
 
-	if ps != &s2 {
+	if ps != s2 {
 		t.Log("the popped ptr should match the one adjusted")
 		t.Fail()
 	}
@@ -93,9 +93,9 @@ func TestPlAdd3AdjustAdjustPop(t *testing.T) {
 	s1 := NewDummySong("Foo", "Bar")
 	s2 := NewDummySong("Nasen", "Baer")
 	s3 := NewDummySong("Hut", "Traeger")
-	pl.Add(&s1)
-	pl.Add(&s2)
-	pl.Add(&s3)
+	pl.Add(s1)
+	pl.Add(s2)
+	pl.Add(s3)
 
 	if len(pl) != 3 {
 		t.Log("len should be 3")
@@ -103,13 +103,13 @@ func TestPlAdd3AdjustAdjustPop(t *testing.T) {
 	}
 
 	s1.Weight += 1
-	pl.Adjust(&s1)
+	pl.Adjust(s1)
 
 	s2.Weight -= 1
-	pl.Adjust(&s2)
+	pl.Adjust(s2)
 
 	s3.Weight += 1
-	pl.Adjust(&s3)
+	pl.Adjust(s3)
 
 	ps := pl.PopSong()
 	if len(pl) != 2 {
@@ -117,7 +117,7 @@ func TestPlAdd3AdjustAdjustPop(t *testing.T) {
 		t.Fail()
 	}
 
-	if ps == &s2 {
+	if ps == s2 {
 		t.Log("the popped ptr is the downvoted one")
 		t.Fail()
 	}
@@ -128,7 +128,7 @@ func TestPlAdd3AdjustAdjustPop(t *testing.T) {
 		t.Fail()
 	}
 
-	if ps == &s2 {
+	if ps == s2 {
 		t.Log("the popped ptr is the downvoted one")
 		t.Fail()
 	}

spotify.go
@@ -96,7 +96,7 @@ func (spotify *SpotifyBackend) Play(song *Song, player Player) {
 	player.PlayData(audioFile)
 }
 
-func (spotify *SpotifyBackend) Search(patterns map[string]string) []Song {
+func (spotify *SpotifyBackend) Search(patterns map[string]string) []*Song {
 	session := spotify.session
 	keyword := ""
 	for _, p := range []string{"pattern", "title", "artist", "album"} {
@@ -110,7 +110,7 @@ func (spotify *SpotifyBackend) Search(patterns map[string]string) []Song {
 
 	if err != nil {
 		llog.Error("Failed to search: %s", err)
-		return []Song{}
+		return nil
 	}
 
 	res := resp.Results
@@ -124,7 +124,7 @@ func (spotify *SpotifyBackend) Search(patterns map[string]string) []Song {
 
 	llog.Debug("\nTracks: %d (total %d)\n", len(res.Tracks.Hits), res.Tracks.Total)
 
-	results := []Song{}
+	results := []*Song{}
 	for _, track := range res.Tracks.Hits {
 		uriParts := strings.Split(track.Uri, ":")
 		s := NewSong(track.Name, track.Artists[0].Name, "spotify", uriParts[len(uriParts)-1])

upload.go
@@ -95,6 +95,6 @@ func (b *UploadBackend) Play(song *Song, player Player) {
 	player.PlayUri("file://" + path.Join(b.uploadDir, song.Uri))
 }
 
-func (b *UploadBackend) Search(map[string]string) []Song {
+func (b *UploadBackend) Search(map[string]string) []*Song {
 	return nil
 }

commit 67b25dea819ea27a9229afebee65e4110c4c6678

fix broken indices in songs after Playlist.OrderedList()
04205039f24ea6e71d2ce94c7cb7e1395f95179e was not enough to
fix the ordered list issue introduced by
26f0033ebb5f8e7c204a042709e3dfa885eac868.
heap.Pop uses Swap internally which modifies as songs index.

We fix the broken indices by overriding them after the ordered list
was created.

playlist.go
@@ -72,6 +72,12 @@ func (pl *Playlist) OrderedList() []*Song {
 		songs = append(songs, heap.Pop(&cpy).(*Song))
 	}
 
+	// TODO: Use more efficiently traversable data structure
+	llog.DDebug("fix song indices in pl")
+	for i, s := range *pl {
+		s.index = i
+	}
+
 	llog.DDebug("ordering queue %v returned %v", pl, songs)
 	return songs
 }

wrms_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"io"
 	"testing"
@@ -48,10 +49,13 @@ func TestWrmsAdd3Next3(t *testing.T) {
 		t.Fail()
 	}
 
-	for _, s := range wrms.queue.OrderedList() {
+	oq := wrms.queue.OrderedList()
+	t.Logf("pl: %v", wrms.queue)
+	t.Logf("oq: %v", oq)
+	for _, s := range oq {
 		wrms.Next()
 		if wrms.CurrentSong.Load().Uri != s.Uri {
-			t.Log("Not returning the next song")
+			t.Logf("Not returning the next song is: %v expected: %v", wrms.CurrentSong.Load(), s)
 			t.Fail()
 		}
 	}
@@ -157,6 +161,30 @@ func TestTimeBonus(t *testing.T) {
 	if wrms.Songs[0].Weight != exp {
 		t.Logf("TimeBonus not correctly applied song %p %v expected: %v",
 			&wrms.Songs[0], wrms.Songs[0], exp)
+		t.Fail()
+	}
+}
+
+func TestMultipleAddsOrderedListUpNext(t *testing.T) {
+	wrms := Wrms{Player: &mockPlayer{}}
+	nsongs := 21
+	songs := make([]*Song, 0, nsongs)
+	for i := 0; i < nsongs; i++ {
+		songs = append(songs, NewDummySong(fmt.Sprintf("song%d", i), "snfmt"))
+	}
+
+	for _, s := range songs {
+		wrms._addSong(s)
+	}
+
+	_ = wrms.queue.OrderedList()
+
+	s := songs[16]
+	wrms.AdjustSongWeight(alice, s.Uri, "up")
+	t.Logf("queue %v", wrms.queue)
+	wrms.Next()
+	if wrms.CurrentSong.Load().Uri != s.Uri {
+		t.Logf("Not playing the upvoted song %v", s)
 		t.Fail()
 	}
 }